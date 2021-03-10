8522f9068706d4943a50f4349e8e3e80d39cce1f
Bruno Sousa <brunosousa@iMac-de-Bruno.local>
2021-03-10 07:55:11 -0300

    Retornar se o número de entrada é divisível por x ou não

---
+++ divisivel_por_x.js
@@ -0,0 +1,9 @@
+function solucao(numero, x) {
+	// seu código aqui
+    var resto = numero % x;
+
+    if (resto === 0) {
+        console.log(true); 
+    } else {
+        console.log(false); }
+}
